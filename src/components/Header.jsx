367e6d8f177c0be84a1fb0e5891caaeadeea9236
username-SARAVANAN <saravanan.my21@gmail.com>
2024-02-19 17:35:50 +0530

    some changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
-import logo from "../assets/investment-calculator-logo.png";
+import headerLogo from "../assets/investment-calculator-logo.png";
 export default function Header() {
   return (
     <header id="header">
-      <img src={logo} alt="picture with money" />
+      <img src={headerLogo} alt="picture with money" />
       <h1>Investment Calculator</h1>
     </header>
   );
