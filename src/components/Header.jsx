017e9e300abcabbb7a3390940dfa5eab150acd70
username-SARAVANAN <saravanan.my21@gmail.com>
2024-02-19 17:34:37 +0530

    changed image path

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,8 @@
+import logo from "../assets/investment-calculator-logo.png";
 export default function Header() {
   return (
     <header id="header">
-      <img src="/investment-calculator-logo.png" alt="picture with money" />
+      <img src={logo} alt="picture with money" />
       <h1>Investment Calculator</h1>
     </header>
   );
